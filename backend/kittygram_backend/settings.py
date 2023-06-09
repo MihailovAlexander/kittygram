00cb28bed52e13cde94fb209da7eb546dabe04fe
MihailovAlexander <1c934567@gmail.com>
2023-06-09 11:48:15 +0700

    remove secrets

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,10 +1,13 @@
 # flake8: noqa
 import os
+from dotenv import load_dotenv()
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('TOKEN', 'django-insecure-123')
 
 DEBUG = True
 
